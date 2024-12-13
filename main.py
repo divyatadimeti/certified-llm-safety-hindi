83edd79ec717b8556be9e4d5886c5432a04b8bbd
Khushi Desai <khushipdesai@gmail.com>
2024-12-12 21:54:41 -0500

    list not allowed wandb

--- main.py
+++ main.py
@@ -850,8 +850,8 @@ elif eval_type == "all_data":
             wandb.log({
                 f"percent_safe_prompt_epoch": percent_safe,
                 f"time_per_prompt_prompt_epoch": time_per_prompt,
-                f"incorrect_predictions": incorrect_indices
             })
+            [wandb.log({f"incorrect_pred{i}": incorrect_indices}) for i in incorrect_indices]
 
         if i + batch_size >= num_prompts:
             # Process the remaining batch
@@ -873,8 +873,8 @@ elif eval_type == "all_data":
                 wandb.log({
                     f"percent_safe_prompt_epoch": percent_safe,
                     f"time_per_prompt_prompt_epoch": time_per_prompt,
-                    f"incorrect_predictions": incorrect_indices
                 })
+                [wandb.log({f"incorrect_pred{i}": incorrect_indices}) for i in incorrect_indices]
 
     print("")
     percent_safe = count_safe / num_prompts * 100
